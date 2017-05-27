7ad61f3cfa219721270597b4c58313cdf4348964
ccc <522978139@qq.com>
2017-05-27 15:29:15 +0800

    不规则布局-修复bug

--- 不规则图片布局/正式版/index.js
+++ 不规则图片布局/正式版/index.js
@@ -1,8 +1,8 @@
 $(function() {
     loadImg(function() {
         let data = getImgData();
-        layout(data,true);
-        $(window).on("resize",function(){
+        layout(data, true);
+        $(window).on("resize", function() {
             layout(data);
         });
     });
@@ -56,11 +56,11 @@ function layout(data, bool) {
         arr.push(data[i].mul);
     }
 
-    let contentWidth = $(".content").width();// 获取大容器宽度,会影响每排多少个图片
-    let max = parseInt(contentWidth/200);//每张图片宽度200px;
-    let groupArr = group(arr, max);//将数据分组
+    let contentWidth = $(".content").width(); // 获取大容器宽度,会影响每排多少个图片
+    let max = parseInt(contentWidth / 200); //每张图片宽度200px;
+    let groupArr = group(arr, max); //将数据分组
 
-    column();//进行每排布局
+    column(); //进行每排布局
 
     bool = bool || false;
     if (bool) { //由于有些情况是布局前会出现滚动条,而布局后,会没了滚动条,容器宽度就会有所变化,所以根据需求是否再次布局
@@ -68,21 +68,21 @@ function layout(data, bool) {
     }
 
     function column() { //每排布局
-        let beginIndex = 0;//开始下标 每排分别对应总图片的第几个开始
-        let lastIndex = 0;//结束下标 每排分别对应总图片的第几个结束
-        let columnMul,h,dataLen;//每排总宽度倍数,每排高度,每排图片数
+        let beginIndex = 0; //开始下标 每排分别对应总图片的第几个开始
+        let lastIndex = 0; //结束下标 每排分别对应总图片的第几个结束
+        let columnMul, h, dataLen; //每排总宽度倍数,每排高度,每排图片数
 
-        for (let i = 0, len = groupArr.length; i < len; i++) {//循环中的每次就是一组,就是一排
-            columnMul = 0;//每排总宽度倍数归0
-            dataLen = groupArr[i].length;//图片数更新
-            lastIndex = beginIndex + dataLen//结束下标 = 开始下标 + 长度
+        for (let i = 0, len = groupArr.length; i < len; i++) { //循环中的每次就是一组,就是一排
+            columnMul = 0; //每排总宽度倍数归0
+            dataLen = groupArr[i].length; //图片数更新
+            lastIndex = beginIndex + dataLen //结束下标 = 开始下标 + 长度
             for (let j = 0; j < dataLen; j++) {
                 columnMul += groupArr[i][j];
             }
 
-            h = count(columnMul, dataLen, contentWidth);//计算每排高度
-            $(".img-wrap").slice(beginIndex,lastIndex).height(h);//找出每排图片,设置高度
-            beginIndex = lastIndex;//这一组的结束下标 , 就是下一组的开始下标
+            h = count(columnMul, dataLen, contentWidth); //计算每排高度
+            $(".img-wrap").slice(beginIndex, lastIndex).height(h); //找出每排图片,设置高度
+            beginIndex = lastIndex; //这一组的结束下标 , 就是下一组的开始下标
         }
     }
 
@@ -113,16 +113,18 @@ function group(data, max) {
 
     for (let i = 0, len = data.length; i < len; i++) {
         columnMul += data[i]; //总宽度递加
-        if (columnMul > +max) { //如果一排总宽度大于6
+        if (columnMul >= max) { //如果一排总宽度大于6
             columnMul -= data[i]; // 则把上面递加的减回来
             columnMul = 0; // 把每排的总宽度归0,因为这已经是属于下一排的总宽度
             secondIndex = 0; //mulArr二级下标归0
-            firstIndex++; //增加排数,所以mulArr二级下标+1
+            if (i != 0) { //这个做个判断,加入第一数据就超出了极限,那么下标就切换到下一排,就会导致第一组数据为空
+                firstIndex++; //切换到下一排,所以mulArr二级下标+1
+            }
         }
         if (secondIndex == 0) { //如果二级下标为0的时候,表明mulArr[firstIndex]的类型还没有定义
             mulArr[firstIndex] = []; //定义为数组
         }
-        mulArr[firstIndex][secondIndex] = data[i]; //放入数据
+        mulArr[firstIndex][secondIndex] = data[i]; //放入数据,这样也保证一排至少有个数据
         secondIndex++;
     }
     return mulArr;
